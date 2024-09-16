396d8294a7fe9da6159f84d8ca0cee75458b9edd
Yash eBizneeds <ygaur@ebizneeds.com>
2024-09-16 16:00:55 +0530

    fixesss

--- src/util/fn.js
+++ src/util/fn.js
@@ -1,59 +1,4 @@
-import { BASEURL } from "../constant/api";
 import moment from "moment"
-const BACKEND_URL = BASEURL.PORT;
-
-export function convertDate(inputFormat) {
-  function pad(s) {
-    return s < 10 ? "0" + s : s;
-  }
-  let d = new Date(inputFormat);
-  return [pad(d.getDate()), pad(d.getMonth() + 1), d.getFullYear()].join("/");
-}
-
-export const addOneToDate = (currDate) => {
-  currDate.setDate(currDate.getDate() + 2);
-  return (currDate = currDate.toISOString().split("T")[0]);
-};
-
-export const intersect = (arr1, arr2) => {
-  for (let i = 0; i < arr1.length; i++) {
-    for (let j = 0; j < arr2.length; j++) {
-      if (arr1[i] === arr2[j]) {
-        return true;
-      }
-    }
-  }
-  return false;
-};
-
-export const getPermissionsObject = (list = []) => {
-  let obj = {};
-  list.forEach(elem => [
-    obj[elem] = true
-  ])
-  return obj
-}
-
-export const isHaveAccess = (key, permissions) => {
-  return !!permissions[key]
-}
-
-export const getLatestCompany = (list = []) => {
-  let latest = {};
-
-  for (let i = 0; i < list.length; i++) {
-    let elem = list[i];
-    if (elem?.is_currently_working == 1) {
-      latest = elem;
-      break;
-    }
-    if (moment(elem.end_date) > moment(latest.end_date)) {
-      latest = elem;
-    }
-  }
-
-  return latest;
-};
 
 export const getTimeGap = (start, end) => {
   let month = moment(end).diff(moment(start), "month");
@@ -87,21 +32,3 @@ export const timeGapYearMonth = (start, end) => {
   }
   return str;
 };
-
-
-// export const getLatestCompany = (list = []) => {
-//   let latest = {};
-
-//   for (let i = 0; i < list.length; i++) {
-//     let elem = list[i];
-//     if (elem?.is_currently_working == 1) {
-//       latest = elem;
-//       break;
-//     }
-//     if (moment(elem.end_date) > moment(latest.end_date)) {
-//       latest = elem;
-//     }
-//   }
-
-//   return latest;
-// };
