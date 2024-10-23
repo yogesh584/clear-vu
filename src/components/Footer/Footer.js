6828650e5ed542ea439517b2803231e4da4e2979
yashgaurr0405 <yashgaur0405@gmail.com>
2024-10-24 01:10:31 +0530

    fixess

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,7 +2,8 @@ import React from "react";
 const Footer = ({posType}) => {
   return (
     <div className="footer bg-white py-4 d-flex flex-lg-column " style={{ display: "none !important", position: posType, bottom: "0px" }}>
-      <div className=" container-fluid  d-flex flex-column flex-md-row align-items-center justify-content-between">
+      {/* justify-content-between ------> add this for shift content to right side */}
+      <div className=" container-fluid  d-flex flex-column flex-md-row align-items-center">
         <div className="text-dark order-2 order-md-1">
           <span className="text-muted mr-2" style={{fontWeight: "normal"}}>Copyright &copy; {new Date().getFullYear()} ClearVu-IQ</span>
           {/* <a target="_blank" className="text-dark-75 text-hover-primary">
